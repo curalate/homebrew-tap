1f4d927397b48bc278ada6743992f868d6193651
Benjamin Chen <benjaminchen@curalate.com>
2016-08-03 12:15:50 -0400

    updates version

--- curacmd.rb
+++ curacmd.rb
@@ -1,8 +1,8 @@
 class Curacmd < Formula
   desc "Curalate Laptop Buildy Stuff"
   homepage "https://github.com/bchen421/curacmd"
-  url "https://github.com/bchen421/curacmd/archive/0.1.2.tar.gz"
-  version "0.1.2"
+  url "https://github.com/bchen421/curacmd/archive/0.1.3.tar.gz"
+  version "0.1.3"
   sha256 "d5bb33175bbffc48cecdb2bdbd59164aae731e1268cdceee1601e6e17fbbb49a"
 
   def install
