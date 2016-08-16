69e09f4f2f44b93ca4390d7faff87e6ac6d89ba9
Benjamin Chen <benjaminchen@curalate.com>
2016-08-16 15:37:45 -0400

    updates sha

--- curacmd.rb
+++ curacmd.rb
@@ -3,7 +3,7 @@ class Curacmd < Formula
   homepage "https://github.com/curalate/curacmd"
   url "https://github.com/curalate/curacmd/archive/0.1.4.tar.gz"
   version "0.1.4"
-  sha256 "1c925e8db38048690aba84cb0f926a0c27a6cd10d88874e5838330d906b5cc74"
+  sha256 "fc193815e3a392c36480dabc2a8e0ad959db10b939b56fa442a9a57740ce9bc0"
 
   def install
     prefix.install [ "lib", "cookbooks", "etc", "var", "data" ]
