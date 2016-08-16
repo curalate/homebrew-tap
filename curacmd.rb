81bd9f7ce03184362d4ece898b79e490bc8686ca
Benjamin Chen <benjaminchen@curalate.com>
2016-08-16 15:35:58 -0400

    0.1.4 release

--- curacmd.rb
+++ curacmd.rb
@@ -1,12 +1,12 @@
 class Curacmd < Formula
   desc "Curalate Laptop Buildy Stuff"
-  homepage "https://github.com/bchen421/curacmd"
-  url "https://github.com/bchen421/curacmd/archive/0.1.3.tar.gz"
-  version "0.1.3"
+  homepage "https://github.com/curalate/curacmd"
+  url "https://github.com/curalate/curacmd/archive/0.1.4.tar.gz"
+  version "0.1.4"
   sha256 "1c925e8db38048690aba84cb0f926a0c27a6cd10d88874e5838330d906b5cc74"
 
   def install
-    prefix.install [ "lib", "cookbooks", "etc", "var", "client.rb" ]
+    prefix.install [ "lib", "cookbooks", "etc", "var", "data" ]
     bin.install "bin/curacmd"
   end
 
