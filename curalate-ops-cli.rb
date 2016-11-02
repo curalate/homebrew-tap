19d8a00c6e647116ded130cd7f9a11a66f44c23d
Benjamin Chen <benjaminchen@curalate.com>
2016-11-02 11:55:48 -0400

    sha update

--- curalate-ops-cli.rb
+++ curalate-ops-cli.rb
@@ -4,7 +4,7 @@ class CuralateOpsCli < Formula
   homepage "https://github.com/curalate/curalate-ops-cli"
   url "https://github.com/bchen421/curalate-ops-cli/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "075ad20cff4014a5946b35e3a2d9ab5f29945d874edcdee8252ccb5ee20cb8b0"
+  sha256 "503aac9d9aaf3cd23f5ffd6f6f4ab501b0733872959709df40fdc92ad09b7fc9"
 
   def install
 
