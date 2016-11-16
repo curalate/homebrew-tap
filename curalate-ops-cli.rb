c5121a0f4d6bad6e9a00a017c0e58fed6c09eb5e
Timothy Hahn <timyhahn@gmail.com>
2016-11-16 16:40:10 -0500

    Forgot to update the actual version!

--- curalate-ops-cli.rb
+++ curalate-ops-cli.rb
@@ -3,7 +3,7 @@ class CuralateOpsCli < Formula
   desc "Curalate Ops Jobs CLI"
   homepage "https://github.com/curalate/curalate-ops-cli"
   url "https://github.com/timothyhahn/curalate-ops-cli/archive/0.0.4.tar.gz"
-  version "0.0.3"
+  version "0.0.4"
   sha256 "df72cc30468ba0eb4e061a84e2b12b53bc8589743b2de96c87bc05febad9eec1"
 
   def install
