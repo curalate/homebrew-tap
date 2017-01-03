21e7a324ffd58e5acb5239e336bb28ab241ecac7
Benjamin Chen <benjaminchen@curalate.com>
2017-01-03 11:23:16 -0500

    cli formula cleanup

--- curalate-ops-cli.rb
+++ curalate-ops-cli.rb
@@ -9,7 +9,7 @@ class CuralateOpsCli < Formula
   def install
 
     prefix.install [ "brew/lib", "brew/include", "brew/.Python", "brew/pip-selfcheck.json", "brew/bin/python" ]
-    bin.install [ "brew/bin/ops", "brew/bin/activate_this.py"
+    bin.install [ "brew/bin/ops", Dir["brew/bin/*"] ]
 
   end
 
