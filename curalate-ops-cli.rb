bb0829ea4216494a8a2cbe38fab4c78f36332961
Timothy Hahn <timyhahn@gmail.com>
2017-01-30 11:15:19 -0500

    Update curalate-ops-cli to 0.0.19
    Includes fixes from https://github.com/curalate/curalate-ops-cli/pull/3.

--- curalate-ops-cli.rb
+++ curalate-ops-cli.rb
@@ -2,9 +2,9 @@ class CuralateOpsCli < Formula
 
   desc "Curalate Ops Jobs CLI"
   homepage "https://github.com/curalate/curalate-ops-cli"
-  url "https://github.com/curalate/curalate-ops-cli/archive/0.0.18.tar.gz"
-  version "0.0.18"
-  sha256 "741457ac986f96aa229f30bed35a62b275bbaf0b0b63a3af585ebb7bddba5f69"
+  url "https://github.com/curalate/curalate-ops-cli/archive/0.0.19.tar.gz"
+  version "0.0.19"
+  sha256 "872fd30e8e95c358001e27642f826ef85fc33f2611709229a7e7c8f60a189dc2"
 
   def install
 
