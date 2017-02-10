4ab5a53e5b7addebcd65b84504ee57d2a3192363
Benjamin Chen <benjaminchen@curalate.com>
2017-02-10 15:11:02 -0500

    0.0.21

--- curalate-ops-cli.rb
+++ curalate-ops-cli.rb
@@ -3,8 +3,8 @@ class CuralateOpsCli < Formula
   desc "Curalate Ops Jobs CLI"
   homepage "https://github.com/curalate/curalate-ops-cli"
   url "https://github.com/curalate/curalate-ops-cli/archive/0.0.20.tar.gz"
-  version "0.0.20"
-  sha256 "3a5f6c6d3c623d0f6170163b92cec62297bbfcac52f44bf5436509979e46ee96"
+  version "0.0.21"
+  sha256 "26c0fadba6edb011293988b05099366fefd9bd8f7dfda63421c006229e03376e"
 
   def install
 
