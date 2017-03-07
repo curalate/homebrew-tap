92a27d1bc78452d5f655b978d3f7e0f5a619092a
Leeroy Jenkins <jenkins-no-reply@curalate.com>
2017-03-07 21:46:31 +0000

    Automagic Jenkins Release 20170307214618

--- curacmd.rb
+++ curacmd.rb
@@ -2,9 +2,9 @@ class Curacmd < Formula
 
   desc "Curalate Laptop Management Stuff"
   homepage "https://github.com/curalate/curacmd"
-  url "https://github.com/curalate/curacmd/archive/20170303212810.tar.gz"
-  version "20170303212810"
-  sha256 "584ff45519798e517b28224e5b5fb135572ee740c567d2c64be7fde9cec66548"
+  url "https://github.com/curalate/curacmd/archive/20170307214618.tar.gz"
+  version "20170307214618"
+  sha256 "9e95fecc15149f2057939232ecec6499b076a831064e303053d5af7c97fdf468"
 
   def install
 
