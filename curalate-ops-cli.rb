ebacfb22ba2930766ca7c81230fcc5dfe44805e9
Rich Schumacher <rich@curalate.com>
2017-05-23 11:56:57 -0400

    Bump curalate-ops-cli dependency to 0.0.23

--- curalate-ops-cli.rb
+++ curalate-ops-cli.rb
@@ -2,8 +2,8 @@ class CuralateOpsCli < Formula
 
   desc "Curalate Ops Jobs CLI"
   homepage "https://github.com/curalate/curalate-ops-cli"
-  url "https://github.com/curalate/curalate-ops-cli/archive/0.0.22.tar.gz"
-  version "0.0.22"
+  url "https://github.com/curalate/curalate-ops-cli/archive/0.0.23.tar.gz"
+  version "0.0.23"
   sha256 "467d9bbc440640dfb524ce46fbe7759475bd98e0296b1194228e30558898f51a"
 
   def install
