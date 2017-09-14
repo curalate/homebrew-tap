ac90fac49c8e2af8af58cc34bf858517006a1fc9
Rich Schumacher <rich@curalate.com>
2017-09-14 13:59:15 -0400

    Bump SHA for curalate-ops-cli 0.0.25

--- curalate-ops-cli.rb
+++ curalate-ops-cli.rb
@@ -4,7 +4,7 @@ class CuralateOpsCli < Formula
   homepage "https://github.com/curalate/curalate-ops-cli"
   url "https://github.com/curalate/curalate-ops-cli/archive/0.0.25.tar.gz"
   version "0.0.25"
-  sha256 "49bac3754b2dfebaea8f5efe229face47437c7548f285731faf0217062e4b3a0"
+  sha256 "425d879b9e6b7bde036b1d88d2468f375ae42769bb3471428e388bfb0f73c338"
 
   def install
 
