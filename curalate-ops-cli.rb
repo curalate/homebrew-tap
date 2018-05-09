665082f5fbbe0f6c7ff79945e70ca80c8195a503
Rich Schumacher <rich@curalate.com>
2018-05-09 10:49:06 -0400

    Force use of curl for retrieving artifact

--- curalate-ops-cli.rb
+++ curalate-ops-cli.rb
@@ -2,7 +2,7 @@ class CuralateOpsCli < Formula
 
   desc "Curalate Ops Jobs CLI"
   homepage "https://github.com/curalate/curalate-ops-cli"
-  url "https://github.com/curalate/curalate-ops-cli/archive/0.0.25.tar.gz"
+  url "https://github.com/curalate/curalate-ops-cli/archive/0.0.25.tar.gz", :using => CurlDownloadStrategy
   version "0.0.25"
   sha256 "425d879b9e6b7bde036b1d88d2468f375ae42769bb3471428e388bfb0f73c338"
 
